3365ea88085a4c0cd35c21455fd5d4ac2cc2811d
Alexander Krasnoyarov <alexander.krasnoyarov@itgalaxy.company>
2014-10-24 01:16:51 +0400

    jshint and remove unused modules

--- test/index.js
+++ test/index.js
@@ -1,49 +1,12 @@
 /*global describe:false, it:false */
+/*jshint expr: true */
+/*jshint immed: false */
+/* exported should */
 var callback = require('./../index');
 
 var should = require('should');
 var File = require('vinyl');
 var es = require('event-stream');
-var gutil = require('gulp-util');
-
-var objectEquals = function(x, y) {
-    if (x instanceof Function) {
-        if (y instanceof Function) {
-            return x.toString() === y.toString();
-        }
-        return false;
-    }
-    if (x === null || x === undefined || y === null || y === undefined) {
-        return x === y;
-    }
-    if (x === y || x.valueOf() === y.valueOf()) {
-        return true;
-    }
-
-    // if one of them is date, they must had equal valueOf
-    if (x instanceof Date) {
-        return false;
-    }
-    if (y instanceof Date) {
-        return false;
-    }
-
-    // if they are not function or strictly equal, they both need to be Objects
-    if (!(x instanceof Object)) {
-        return false;
-    }
-    if (!(y instanceof Object)) {
-        return false;
-    }
-
-    var p = Object.keys(x);
-    return Object.keys(y).every(function (i) {
-        return p.indexOf(i) !== -1;
-    }) ?
-        p.every(function (i) {
-            return objectEquals(x[i], y[i]);
-        }) : false;
-};
 
 describe('gulp-callback', function() {
     it('should works', function(done) {
