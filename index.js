de0335c9984bbdb923741279c5c054fe057a8e91
Alexander Krasnoyarov <alexander.krasnoyarov@itgalaxy.company>
2014-10-23 20:32:17 +0400

    #2

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
 var through = require('through2');
+var gutil = require('gulp-util');
 
 module.exports = function (fn) {
     return through.obj(function (file, enc, cb) {
-        if (typeof(v) === 'function') {
-            fn(cb(file, null));
+        if (typeof(fn) === 'function') {
+            return fn(file, enc, cb(file, null));
+        } else {
+            return cb(new gutil.PluginError('gulp-callback', 'Callback is not function'));
         }
     });
 };
