8c6a886001b4524855df849652d4dc6c8ddc8614
Alexander Krasnoyarov <alexander.krasnoyarov@itgalaxy.company>
2014-10-23 21:00:33 +0400

    #3

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 'use strict';
 var through = require('through2');
 var gutil = require('gulp-util');
+var defaults = require('lodash.defaults');
 
-module.exports = function (fn) {
+module.exports = function (fn, options) {
+    options = defaults(options || {}, {
+        once: false
+    });
+    var once = false;
+
     return through.obj(function (file, enc, cb) {
+        var callback = function () {
+            if (options.once) {
+                once = true;
+            }
+            cb();
+        };
+
         if (typeof(fn) === 'function') {
-            return fn(file, enc, cb(file, null));
+            if (!once) {
+                return fn(file, enc, callback);
+            } else {
+                cb(null, file);
+            }
         } else {
             return cb(new gutil.PluginError('gulp-callback', 'Callback is not function'));
         }
